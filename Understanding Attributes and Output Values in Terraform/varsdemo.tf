05be7e0abba010ace8d3bbe6cfea54cb6f7c7b32
gauravbhalekar5 <61308032+gauravbhalekar5@users.noreply.github.com>
2023-03-30 21:18:01 +0530

    Update varsdemo.tf

--- Understanding Attributes and Output Values in Terraform/varsdemo.tf
+++ Understanding Attributes and Output Values in Terraform/varsdemo.tf
@@ -1,4 +1,3 @@
-/*
 provider "aws" {
     region = "us-east-1"
     access_key = "PUT-YOUR-ACCESS-KEY-HERE"
@@ -29,9 +28,11 @@ resource "aws_security_group" "var-demo" {
     protocol = "tcp"
     cidr_blocks = [var.vpn_ip]
   }
-}*/
+}
 
-/*
+
+
+
 This code uses the AWS provider to create a security group named "evolent". 
 This security group allows inbound traffic on ports 443, 80 and 8080 from a specific IP address provided in a variable called "vpn_ip". 
 The AWS provider is configured with the access and secret keys to access resources in the us-east-1 region.
@@ -39,4 +40,4 @@ The AWS provider is configured with the access and secret keys to access resourc
 This is a Terraform configuration file for the AWS provider. It sets the region to "us-east-1" and authenticates with access and secret keys.
 It creates an AWS security group resource named "var-demo" with three ingress rules. Each ingress rule specifies a port range, a transport protocol (TCP), and a CIDR block to allow traffic from (specified here as a variable called "vpn_ip").
 The first rule permits inbound traffic on port 443 (HTTPS), the second on port 80 (HTTP), and the third permits inbound traffic on port 8080. All inbound traffic is restricted to the CIDR block specified in the variable "vpn_ip".
-*/
+
