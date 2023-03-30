00e0786af58e0543abe7eb31bd8ece66e089bb45
gauravbhalekar5 <61308032+gauravbhalekar5@users.noreply.github.com>
2023-03-30 21:17:22 +0530

    Update variables.tf

--- Understanding Attributes and Output Values in Terraform/variables.tf
+++ Understanding Attributes and Output Values in Terraform/variables.tf
@@ -1,20 +1,21 @@
-/*
+
 variable "vpn_ip" {
   default = "10.10.10.100/32"
-}*/
+}
 
-/*
+
 This code is defining a variable called "vpn_ip" with a default value of "10.10.10.100/32". 
 This variable can be used elsewhere in the code as needed.
 
 This code defines a variable named "vpn_ip" and sets its default value to "10.10.10.100/32". 
 This variable can be used in your Terraform code to reference the IP address and subnet mask for a VPN connection.
-*/
 
-/*
+===============================================================================================================================
+    
+
 variable "usernumber" {
   type = number
-}*/
+}
 
 variable "elb_name" {
   type = string
