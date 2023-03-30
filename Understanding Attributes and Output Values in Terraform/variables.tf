0b899c2fac4a4d7b71291deac900e92d75a61220
gauravbhalekar5 <61308032+gauravbhalekar5@users.noreply.github.com>
2023-03-30 21:30:39 +0530

    Update variables.tf

--- Understanding Attributes and Output Values in Terraform/variables.tf
+++ Understanding Attributes and Output Values in Terraform/variables.tf
@@ -10,7 +10,7 @@ This variable can be used elsewhere in the code as needed.
 This code defines a variable named "vpn_ip" and sets its default value to "10.10.10.100/32". 
 This variable can be used in your Terraform code to reference the IP address and subnet mask for a VPN connection.
 
-===============================================================================================================================
+
     
 
 
